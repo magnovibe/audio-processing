58efa0eb1da2e24e7ae4d6070744b06e3271905d
alexandra somodi <asomodi@usc.edu>
2023-11-27 21:42:18 -0800

    Update beatfinder.py

--- beatfinder.py
+++ beatfinder.py
@@ -2,6 +2,9 @@
 # https://librosa.org/doc/0.10.1/generated/librosa.display.waveshow.html#librosa.display.waveshow
 
 import librosa
+import subprocess
+import serial
+import time
 import librosa.display
 import matplotlib.pyplot as plt
 
@@ -13,20 +16,9 @@ tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
 
 print(f"Tempo: {tempo} BPM")
 
-with open('tempo.txt', 'w') as file:
-    file.write(str(tempo))
+#preprocTemp = subprocess.check_output(['python3', 'beatfinder.py']).decode('utf-8').strip()
+myData = "HI ARDUINO!"
+ser = serial.Serial('/dev/cu.usbmodem*',9600) # needs to be changed to COM3 on windows
+ser.write(myData.encode('utf-8'))
 
-#plt.figure(figsize=(15, 4))
-#fig, ax = plt.subplots()
-#plt.vlines(librosa.frames_to_time(beats), -1, 1, color='r', alpha=0.7, label='Beats')
-#plt.legend()
-#plt.show()
-
-
-#fig, ax = plt.subplots(nrows=3, sharex=True)
-#y_harm, y_perc = librosa.effects.hpss(y)
-#librosa.display.waveshow(y_harm, sr=sr, alpha=0.5, ax=ax[2], label='Harmonic')
-#librosa.display.waveshow(y_perc, sr=sr, color='r', alpha=0.5, ax=ax[2], label='Percussive')
-#ax[2].set(title='Multiple waveforms')
-#ax[2].legend()
-
+ser.close()
