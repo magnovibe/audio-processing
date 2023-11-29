ef25f3c4c9b7a628d7d80da8e5dae8d947113d77
alexandra somodi <asomodi@usc.edu>
2023-11-29 00:29:13 -0800

    Update beatfinder.py

--- beatfinder.py
+++ beatfinder.py
@@ -7,7 +7,15 @@ import serial
 import time
 import librosa.display
 import matplotlib.pyplot as plt
+import serial.tools.list_ports 
+# initialize serial stuff
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
 
+portList = []
+count = 0
+
+# extract tempo
 audio_file = "jazztheme.mp3"
 y, sr = librosa.load(audio_file)
 
@@ -16,9 +24,22 @@ tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
 
 print(f"Tempo: {tempo} BPM")
 
-#preprocTemp = subprocess.check_output(['python3', 'beatfinder.py']).decode('utf-8').strip()
-myData = "HI ARDUINO!"
-ser = serial.Serial('/dev/cu.usbmodem*',9600) # needs to be changed to COM3 on windows
-ser.write(myData.encode('utf-8'))
+#establish serial connection
+for onePort in ports: 
+    portList.append(str(onePort))
+    count += 1
+    print(str(count) + ": " + str(onePort))
 
-ser.close()
+val = input("Select port: ")
+split_val = portList[int(val) - 1].split()
+port_name = split_val[0]
+print(port_name) # portList[int(val) - 1])
+
+
+serialInst.baudrate = 9600 # Needs to match the baud rate in the arduino code
+serialInst.port = port_name 
+print("about to open")
+serialInst.open()
+print("port opened") 
+serialInst.write(str(tempo).encode('utf-8'))
+serialInst.close()
