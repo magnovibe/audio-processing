e1819710c3a25eed3580e8ee05104b4cf39d850b
alexandra somodi <asomodi@usc.edu>
2023-11-30 00:34:06 -0800

    allegedly working mag code!

--- emagTest.ino
+++ emagTest.ino
@@ -2,6 +2,9 @@ volatile int pwm_width = 4000;
 uint16_t tempo = 0;
 volatile float myTempo = 0;
 volatile float timePerBeat = 0;
+volatile float timeOn = 0;
+volatile float timeOff = 0;
+volatile int readyToCook = 0;
 void setup() {
   // put your setup code here, to run once:
   // put your main code here, to run repeatedly:
@@ -9,43 +12,45 @@ void setup() {
 
   // PWM width will vary depending on loudness, for now it's constant
 
-  // initialize the digital pin as an output.
-    // pinMode(Electromagnet, OUTPUT);
+    // set IN3 to 1, IN4 already grounded
+    DDRD |= (1 << PD7);
+    PORTD |= (1 << PD7);
 
-    // set OCR1B bit as an output
-    DDRB |= (1 << PB2) | (1 << PB5);
-
+    // turn on ENA bit (for PWM) initially
+    DDRB |= (1 << PB2);
+    PORTB |= (1 << PB2);
     Serial.begin(9600);
 
-    // enable global interrupts
-    sei();
-
-    //timer1_init();
-    PORTB |= (1 << PB2);
-    PORTB |= (1 << PB5);
     
 }
 
 void loop() {
- if (Serial.available() > 0) {
-    String pwm = Serial.readString();
-    Serial.print(pwm);
-    myTempo = pwm.toFloat();
-    timePerBeat = 60 / myTempo;
-    OCR1A = 16000000 * timePerBeat / 1024;
-    OCR1B = OCR1A * 0.8;
+ int magnetPin = 10;
+
+ // code for serial... not working rn for some reason
+// if (Serial.available() > 0) {
+//    PORTB &= !(1 << PB2);
+//    String pwm = Serial.readString();
+//    delay(4000);
+//    Serial.print(pwm);
+//    Serial.end();
+//    myTempo = pwm.toFloat();
+//    timePerBeat = 60 / myTempo;
+//    timeOn = timePerBeat * 0.8;
+//    timeOff = timePerBeat - timeOn;
+//    readyToCook = 1;
+// }
+
+if (readyToCook == 0) {
+  myTempo = 117.453835;
+  timePerBeat = 60 / myTempo;
+  timeOn = timePerBeat * 0.8;
+  timeOff = timePerBeat - timeOn;
+  readyToCook = 1;
  }
-  
-}
-
-
-void timer1_init() {
-  TCCR1B |= (1 << WGM13) | (1 << WGM12);
-  TCCR1B |= (1 << CS10) | (1 << CS12);
-  TCCR1A |= (1 << WGM11) | (WGM10);
-  TCCR1A |= (1 << COM1B1);
-  OCR1A = 40000;
-
-  // PB2 (D10) = PULSE OUTPUT
-
+ if (readyToCook == 1) {
+  analogWrite(magnetPin, 255);
+  delay(timeOn * 1000);
+  analogWrite(magnetPin, 80);
+  delay(timeOff * 1000);
 }
