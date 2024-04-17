853ee1f29698f7d5a9c314243303371ee9942f07
alexandra somodi <asomodi@usc.edu>
2024-04-16 23:36:25 -0700

    Update beatfinder.py

--- beatfinder.py
+++ beatfinder.py
@@ -1,15 +1,13 @@
 # audio download from pixabay
 # https://librosa.org/doc/0.10.1/generated/librosa.display.waveshow.html#librosa.display.waveshow
 
+import librosa
 import subprocess
 import serial
 import time
+import librosa.display
 import matplotlib.pyplot as plt
 import serial.tools.list_ports 
-
-# including audioClass
-from audioClass import Audio
-
 # initialize serial stuff
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
@@ -17,20 +15,14 @@ serialInst = serial.Serial()
 portList = []
 count = 0
 
-# for getting audio file initialized in code
-audio = input("Enter audio file: ")
-song = Audio(audio)
-tempo = song.beatTempo
-vol = song.beatVolume
-beats = song.beatStep
-# general testing
-print("Tempo: " + str(tempo))
-print("Volume: " + str(vol))
-print("Beats: " + str(beats))
-song.magStrengthVol(vol, 2)
+# extract tempo
+audio_file = "jazztheme.mp3"
+y, sr = librosa.load(audio_file)
 
-for i in range(20):
-    song.magStrengthVol(vol, i)
+onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
+
+print(f"Tempo: {tempo} BPM")
 
 
 #establish serial connection
@@ -49,6 +41,7 @@ serialInst.baudrate = 9600 # Needs to match the baud rate in the arduino code
 serialInst.port = port_name 
 print("about to open")
 serialInst.open()
+print("openedish")
 pause = input()
 print("port opened") 
 serialInst.write(str(tempo).encode('utf-8'))
